7b0a1a7ad30499a4753a0b92cf5b054f8e4b5a52
Pedro Jiménez <pheetbag@gmail.com>
2018-07-01 16:17:22 -0400

    <a> corregidos

--- vistas/registrar/index.php
+++ vistas/registrar/index.php
@@ -14,7 +14,7 @@
                 <h6 class="card-header text-center">Usuario</h6>
                 <div class="card-body text-primary">
                     <p class="card-text">Registra un nuevo usuario, para permitir el acceso al sistema desde una cuenta nueva.</p>
-                    <a class="btn btn-primary mt-3 d-block" href="/registrar/usuario">Registrar</a>
+                    <a class="btn btn-primary mt-3 d-block" href="registrar/usuario">Registrar</a>
                 </div>
             </div>
 
@@ -27,7 +27,7 @@
                 <h6 class="card-header text-center">Cliente</h6>
                 <div class="card-body text-primary">
                     <p class="card-text">Registra un nuevo cliente en el sistema, para generar asociadas al cliente.</p>
-                    <a class="btn btn-primary mt-3 d-block" href="/registrar/cliente">Registrar</a>
+                    <a class="btn btn-primary mt-3 d-block" href="registrar/cliente">Registrar</a>
                 </div>
             </div>
 
@@ -45,7 +45,7 @@
                 <h6 class="card-header text-center">Proveedor</h6>
                 <div class="card-body text-primary">
                     <p class="card-text">Registra un nuevo proveedor en el sistema, para generarle nuevos pedidos a dicho proveedor.</p>
-                    <a class="btn btn-primary mt-3 d-block" href="/registrar/proveedor">Registrar</a>
+                    <a class="btn btn-primary mt-3 d-block" href="registrar/proveedor">Registrar</a>
                 </div>
             </div>
 
@@ -58,7 +58,7 @@
                 <h6 class="card-header text-center">Producto</h6>
                 <div class="card-body text-primary">
                     <p class="card-text">Registra un nuevo producto en el sistema, para poder inventariarlo, venderlo y generar pedidos.</p>
-                    <a class="btn btn-primary mt-3 d-block" href="/registrar/producto">Registrar</a>
+                    <a class="btn btn-primary mt-3 d-block" href="registrar/producto">Registrar</a>
                 </div>
             </div>
 
@@ -73,7 +73,7 @@
                 <h6 class="card-header text-center">Factura</h6>
                 <div class="card-body text-primary">
                     <p class="card-text">Registra una nueva factura, para vender productos del inventario.</p>
-                    <a class="btn btn-primary mt-3 d-block" href="/registrar/factura">Registrar</a>
+                    <a class="btn btn-primary mt-3 d-block" href="registrar/factura">Registrar</a>
                 </div>
             </div>
 
@@ -86,7 +86,7 @@
                 <h6 class="card-header text-center">Pedido</h6>
                 <div class="card-body text-primary">
                     <p class="card-text">Registra un pedido para reponer las existencias, comprando a un proveedor.</p>
-                    <a class="btn btn-primary mt-3 d-block" href="/registrar/pedido">Registrar</a>
+                    <a class="btn btn-primary mt-3 d-block" href="registrar/pedido">Registrar</a>
                 </div>
             </div>
 
