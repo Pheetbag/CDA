8af202eca744b536532f8d8525755615718b481f
Pedro Jiménez <pheetbag@gmail.com>
2018-07-15 18:52:11 -0400

    added offline support

--- libs/includes.php
+++ libs/includes.php
@@ -11,7 +11,7 @@ function include_head($title = 'Sistema CDA'){
             <meta http-equiv="X-UA-Compatible" content="ie=edge">
             <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
             <title>'. $title .'</title>
-            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+            <link rel="stylesheet" href="'    . HTTP . '/recursos/libs/bootstrap/css/bootstrap.min.css">
             <link rel="shortcut icon" href="' . HTTP . '/recursos/cda-logosmall.png" type="image/png" />
             <link rel="stylesheet" href="'    . HTTP . '/vistas/defaults/defaults.css?v=0.15">
 
@@ -174,13 +174,13 @@ function include_header($activo = null, $title = 'Titulo', $subtitle = ''){
 function include_footer($extra = ''){
     echo '
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="' . HTTP . '/recursos/libs/jquery.min.js"></script>
+	<script src="' . HTTP . '/vistas/defaults/cookies.js"></script>
 
-    <script src="' . HTTP . '/vistas/defaults/cookies.js"></script>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="' . HTTP . '/recursos/libs/popper.min.js"></script>
+	<script src="' . HTTP . '/recursos/libs/bootstrap/js/bootstrap.min.js"></script>
 
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
 
     <script src="' . HTTP . '/vistas/defaults/defaults.js?v=0.5"></script>
     ' . $extra . '
